7cf8e2c431e2986dbdf7096190d33f9300bcd439
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-01-04 09:58:59 +0100

    TP 3 Q1

--- hangman.php
+++ hangman.php
@@ -2,12 +2,17 @@
 
 $possibilities = ['mystere', 'titi', 'toto'];
 $myst = $possibilities[rand(0, count($possibilities) - 1)];
+$equals = false;
 
-do {
-    echo "Try to find '$myst': ";
+for ($life = 5; !$equals && $life > 0; -- $life) {
+    echo "Try to find '$myst' (you have $life lifes): ";
     $handle = fopen('php://stdin', 'r');
     $line = strtolower(trim(fgets($handle)));
     $equals = $line == $myst;
 
     printf('You try "%s", that\'s %scorrect%s', $line, ($equals ? '' : 'in'), PHP_EOL);
-} while (!$equals);
+};
+
+if (!$equals) {
+    echo 'You lose!' . PHP_EOL;
+}
